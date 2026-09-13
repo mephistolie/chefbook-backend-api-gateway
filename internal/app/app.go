3a5a0105af98008213e7e90ae049e516c530b1a4
mephistolie <mikhaillevin@inbox.ru>
2026-09-14 02:01:41 +0300

    fix(gateway): use configured backend domain in Swagger

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	"github.com/mephistolie/chefbook-backend-api-gateway/docs"
 	"github.com/mephistolie/chefbook-backend-api-gateway/internal/config"
 	eventlog "github.com/mephistolie/chefbook-backend-api-gateway/internal/logging"
 	"github.com/mephistolie/chefbook-backend-api-gateway/internal/server"
@@ -19,6 +20,7 @@ import (
 )
 
 func Run(cfg *config.Config) {
+	docs.SwaggerInfo.Host = *cfg.Domains.Backend
 	log.InitWithService("api-gateway", *cfg.LogsPath, *cfg.Environment == config.EnvDev)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
